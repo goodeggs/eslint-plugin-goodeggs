f612bc13e2abb2d161ae5ec8e7e56683deea14f7
Nathan Houle <nathan@nathanhoule.com>
2019-01-30 11:46:48 -0800

    Break ops-specific configuration into its own config

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,6 @@
 export {default as flowtype} from './flowtype';
 export {default as jest} from './jest';
 export {default as mocha} from './mocha';
+export {default as ops} from './ops';
 export {default as react} from './react';
 export {default as recommended} from './recommended';
