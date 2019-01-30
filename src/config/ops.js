72a62502cc87c10874dfbc98c7e8c501bff9f68b
Nathan Houle <nathan@nathanhoule.com>
2019-01-30 11:46:48 -0800

    Break ops-specific configuration into its own config

---
+++ src/config/ops.js
@@ -0,0 +1,11 @@
+import recommended from './recommended';
+
+export default {
+  rules: {
+    'camelcase': ['error', {'allow': ['^DT_.+']}],
+    'new-cap': ['error', {
+      ...recommended.rules['new-cap'][1],
+      'capIsNewExceptionPattern': '(dryrain.+|window)\\.DT_.+'
+    }]
+  },
+};
