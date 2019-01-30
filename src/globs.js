f6955526527eec700212ee230ad8613e188597e0
Nathan Houle <nathan@nathanhoule.com>
2019-01-30 09:40:00 -0800

    WIP: Upgrade deps, start testing against projects
    - Start introducing eslint-plugin-babel
    - Upgrade eslint-plugin-import

---
+++ src/globs.js
@@ -0,0 +1,18 @@
+/*
+ * Common globs used across all Good Eggs projects.
+ */
+
+export const extensions = ['js', 'jsx', 'ts', 'tsx'];
+
+export const src = {
+  // All non-test source files.
+  all: `**/!(?(*.)test?(.*)).{${extensions.join(',')}}`,
+};
+
+export const test = {
+  // All test files of all scopes.
+  all: `**/?(*.)test.?(*.){${extensions.join(',')}}`,
+  e2e: `**/?(*.)test.e2e.{${extensions.join(',')}}`,
+  smoke: `**/?(*.)test.smoke.{${extensions.join(',')}}`,
+  unit: `**/?(*.)test.!(*.){${extensions.join(',')}}`,
+};
