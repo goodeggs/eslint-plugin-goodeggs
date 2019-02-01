d07f19a434e81ff6fe9fd1a7d08ef880b3c4382e
Nathan Houle <nathan@nathanhoule.com>
2019-02-01 00:25:55 -0800

    Disable prop-types in react plugin

--- src/config/react.js
+++ src/config/react.js
@@ -1,4 +1,8 @@
 export default {
   plugins: ['prettier', 'react'],
   extends: ['plugin:import/react', 'plugin:react/recommended', 'prettier/react'],
+  rules: {
+    // We leverage typing via Flow and TypeScript, which are redundant with prop types.
+    'react/prop-types': 'off',
+  },
 };
