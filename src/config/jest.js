c5277df0ae472de9ad1077461b54747150a0a5f5
Nathan Houle <nathan@nathanhoule.com>
2019-02-01 13:13:10 -0800

    Enable additional Jest rules, add strict Jest config

--- src/config/jest.js
+++ src/config/jest.js
@@ -4,10 +4,7 @@ export default {
   plugins: ['jest'],
   extends: ['plugin:jest/recommended'],
   rules: {
-    // TODO(ndhoule): This is a good rule, but it's difficult to use in repositories that use a
-    // combination of Jest and Chai `expect`s. We need to develop an easier migration path for
-    // assertions before we can turn this on globally. In the meantime, Flow and TS generally catch
-    // invalid assertions.
+    'jest/prefer-spy-on': 'warn',
     'jest/valid-expect': 'off',
   },
   overrides: [
