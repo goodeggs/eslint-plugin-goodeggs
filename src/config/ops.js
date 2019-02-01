6bb06482085f3cd5c19215eaab268a81604cb81a
Nathan Houle <nathan@nathanhoule.com>
2019-02-01 00:00:39 -0800

    Fix dryrain new cap regexp, handle `unionized.JSONSchemaFactory`

--- src/config/ops.js
+++ src/config/ops.js
@@ -1,13 +1,21 @@
 import recommended from './recommended';
 
+const existingNewCapRule = recommended.rules['new-cap'][1];
+
 export default {
   rules: {
     camelcase: ['error', {allow: ['^DT_.+']}],
     'new-cap': [
       'error',
       {
-        ...recommended.rules['new-cap'][1],
-        capIsNewExceptionPattern: '(dryrain.+|window)\\.DT_.+',
+        ...existingNewCapRule,
+        capIsNewExceptionPattern: [
+          '(',
+          ['(dryrain.*|window)\\.DT_.+', existingNewCapRule.capIsNewExceptionPattern]
+            .map((regexp) => `(${regexp})`)
+            .join('|'),
+          ')',
+        ].join(''),
       },
     ],
   },
