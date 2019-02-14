15a83689771d70d887d88b21272e225f821a1197
Nathan Houle <nathan@nathanhoule.com>
2019-02-14 11:17:23 -0800

    Enable more Mocha rules

--- src/config/mocha.js
+++ src/config/mocha.js
@@ -1,4 +1,13 @@
+// https://github.com/lo1tuma/eslint-plugin-mocha
 export default {
   plugins: ['mocha'],
   extends: ['plugin:mocha/recommended'],
+  rules: {
+    'mocha/handle-done-callback': 'error',
+    'mocha/no-async-describe': 'error',
+    'mocha/no-identical-title': 'error',
+    'mocha/no-nested-tests': 'error',
+    'mocha/no-return-and-callback': 'error',
+    'mocha/no-top-level-hooks': 'error',
+  },
 };
