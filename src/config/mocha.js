766dd8ddbeea7f3c8c68a69a33fd9fe21b8b2350
Nathan Houle <nathan@nathanhoule.com>
2019-11-11 14:38:03 -0800

    chore: update plugins, typescript, prettier deps
    This changeset updates all `eslint-plugin-` dependencies and any of
    their peers (i.e. `prettier` and `typescript`).
    
    This enables a couple new rules, and some of the plugins themselves had
    breaking changes (a couple rule renames, but mostly changes to their
    recommended rulesets) so this is a breaking change.
    
    This changeset also loosens the peer dependency version around
    `typescript` to exactly match the version required by
    `@typescript-eslint/parser`. It does the same thing with `prettier` to
    match the peer dependency specified by `eslint-plugin-prettier`.

--- src/config/mocha.js
+++ src/config/mocha.js
@@ -3,11 +3,18 @@ export default {
   plugins: ['mocha'],
   extends: ['plugin:mocha/recommended'],
   rules: {
-    'mocha/handle-done-callback': 'error',
-    'mocha/no-async-describe': 'error',
-    'mocha/no-identical-title': 'error',
-    'mocha/no-nested-tests': 'error',
-    'mocha/no-return-and-callback': 'error',
+    // TODO(ndhoule): Enabled in `mocha/recommended`. This is a de facto best practice, but we
+    // haven't codified it anywhere; consider enabling this rule.
+    'mocha/max-top-level-suites': 'off',
+    // TODO(ndhoule): This rule is great, but it autofix feature is broken in a way that dumps the
+    // fixed content at the top of the file, resulting in a syntax error. When it's fixed upstream,
+    // re-enable this.
+    'mocha/no-mocha-arrows': 'off',
+    // Enabled in `mocha/recommended`; contradicts our best practices.
+    'mocha/no-setup-in-describe': 'off',
+    // Enabled in `mocha/recommended`; contradicts our best practices which dictate that we prefer
+    // multiple small/focused hooks over one big one that does many things.
+    'mocha/no-sibling-hooks': 'off',
     'mocha/no-top-level-hooks': 'error',
   },
 };
