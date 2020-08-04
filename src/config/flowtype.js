f9038825996b50213afcef9653884f36741c8633
Philippe Serhal <philippe@goodeggs.com>
2020-08-04 15:10:24 -0400

    fix: work around eslint plugin sharing limitation
    See https://www.npmjs.com/package/@rushstack/eslint-patch for a full history.
    
    See https://goodeggs.slack.com/archives/CPX6NFU02/p1586283695076400 and
    https://goodeggs.slack.com/archives/CPX6NFU02/p1588966563372800 for some past discussions
    on the topic.
    
    I generally avoid monkey-patches like the plague, but:
    - all alternatives here are terrible, are high cost/effort, and require adjustments in
      every single repo that uses eslint-plugin-goodeggs
    - this is causing significant developer pain
    - this is a tracked issue in an active, maintained community library (eslint) which has a
      specific roadmap to address the underlying issue
    - this is a community-maintained monkey-patch used by many repos, and owned by Microsoft

--- src/config/flowtype.js
+++ src/config/flowtype.js
@@ -1,3 +1,5 @@
+import '@rushstack/eslint-patch/modern-module-resolution';
+
 export default {
   plugins: ['flowtype', 'prettier'],
   extends: ['plugin:flowtype/recommended', 'prettier/flowtype'],
