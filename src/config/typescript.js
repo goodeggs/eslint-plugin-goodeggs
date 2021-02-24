e55fb84324a2cc3b55e95c39d151df6a6dfb9552
Marco Aurelio Maturana Silveira <marco.maturana.ro@gmail.com>
2021-02-24 07:20:21 -0300

    add @typescript-eslint/prefer-includes

--- src/config/typescript.js
+++ src/config/typescript.js
@@ -118,6 +118,7 @@ export default {
         ],
         '@typescript-eslint/switch-exhaustiveness-check': 'error',
         '@typescript-eslint/array-type': ['error', {default: 'array-simple'}],
+        '@typescript-eslint/prefer-includes': 'error',
       },
     },
   ],
