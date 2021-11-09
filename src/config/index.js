70aaf656e31801ca7817c53db318ebf9a5e3c25a
Wagner Shimatai <wagner.shimatai@goodeggs.com>
2021-11-09 13:35:53 -0300

    Jest strict file renamed to satify filename rule.

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export {default as flowtype} from './flowtype';
-export {default as jestStrict} from './jest-strict';
+export {default as jestStrict} from './jest_strict';
 export {default as jest} from './jest';
 export {default as mocha} from './mocha';
 export {default as ops} from './ops';
